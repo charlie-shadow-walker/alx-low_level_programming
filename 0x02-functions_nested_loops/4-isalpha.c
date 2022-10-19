df1844510b29c67e558bdc694c80e38ff2b2b3dd
charlie-shadow-walker <mastercharlie1998@gmail.com>
2022-10-19 06:55:26 -0700

    functions and loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,11 +5,11 @@
  * Return: 1 is c if true else 0
  */
 
-int _isalpha(int c);
+int _isalpha(int c)
 {
 	if (c >= 97 && c <= 122 && c >= 65 && c >= 90)
 	{
-		return (0);
+		return (1);
 	}
 	return (0);
 }
