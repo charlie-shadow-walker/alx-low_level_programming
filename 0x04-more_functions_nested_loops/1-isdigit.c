34d6cd053bc86d8b968c64db0cb41ee15866fced
charlie-shadow-walker <mastercharlie1998@gmail.com>
2022-10-21 11:42:52 -0700

    functions

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * _isgidit - checks for input
+ * @c: input
+ * return: 1 if digit, 0 if not
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
