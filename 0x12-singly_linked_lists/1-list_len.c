4ddfac18a71be405fdd02918d003e3652374704a
charlie-shadow-walker <mastercharlie1998@gmail.com>
2022-11-28 05:09:23 -0800

    prints elements in a list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - prints the number of elements in list_t
+ *
+ * @h: pointer to the head
+ *
+ * Return: returns number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t n = 1;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		n++
+	}
+	return (n);
+}
