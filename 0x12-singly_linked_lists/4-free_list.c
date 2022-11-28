d1ad45e33c81c0df8274a1834e9fcad2f39873a3
charlie-shadow-walker <mastercharlie1998@gmail.com>
2022-11-28 13:58:11 -0800

    changes

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * free_list - a function that frees a list_t
+ *
+ * @head: pointer to the first node
+ *
+ * Returns: nothing
+ */
+
+void free_list(list_t *head)
+{
+
+}
