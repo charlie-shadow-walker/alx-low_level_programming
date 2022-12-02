1a2c8d0d11a51fbd33eec29c01b5605a9ac7828f
charlie-shadow-walker <mastercharlie1998@gmail.com>
2022-12-02 12:11:54 -0800

    changes

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -15,6 +15,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *temp;
 
 	lastNode = malloc(sizeof(listint_t));
+	temp = malloc(sizeof(listint_t));
 
 	if (lastNode == NULL)
 	{
