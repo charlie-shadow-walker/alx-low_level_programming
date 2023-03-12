97451ce626dae1f0d1ba4fd219273575e0006465
charlie-shadow-walker <mastercharlie1998@gmail.com>
2023-03-12 10:29:33 +0300

    function pointers

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_iterator - print elements in an array
+ * @array: the array
+ * @size: the size of the array
+ * @action: pointer to the function to use
+ * Return: nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+	if (!array || !action)
+		return;
+	for (i = 0; i< (int) size; i++)
+		action(array[i]);
+}
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,32 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * int_index - searches for an integer
+ * @array: the array
+ * @size: of the array
+ * @cmp: pointer to the fucntion
+ * Return: -1 if size <= 0;
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <=0)
+	{
+		return(-1);
+	}
+
+	if (array == NULL || cmp == NULL)
+	{
+		return (-1);
+	}
+
+	for (i =1; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1)
+}
