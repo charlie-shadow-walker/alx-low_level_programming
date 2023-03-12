1abeb43ed11cb9369777a74a47a5000c3ab6b58a
charlie-shadow-walker <mastercharlie1998@gmail.com>
2023-03-12 10:53:18 +0300

    double linked list' git push

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - add node at end of a linked list
+ * @head: the head node to the linked list
+ * @n: data to be inserted in the dll
+ *
+ * Return: return pointer to the new node
+ * The time complexity of this program is O(n)
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node, *temp;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	temp = *head;
+
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+
+	temp->next = new_node;
+	new_node->prev = temp;
+
+	return (new_node);
+}
